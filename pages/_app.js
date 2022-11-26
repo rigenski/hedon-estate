9dde2ac9d19459543eca8646041c6f92fddc6968
Rigen Maulana <rygen.dev@gmail.com>
2022-11-26 11:09:04 +0700

    feat: hidden aos

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@ import "bootstrap/dist/css/bootstrap.css";
 import { useEffect } from "react";
 import "@/styles/globals.css";
 import "@/styles/components.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
+
+    AOS.init();
   }, []);
 
   return <Component {...pageProps} />;
